7746d6a836af647c16d8834e5563c7d53896f69e
danneggiasta <zlatanovic.nebojsa@yahoo.com>
2017-09-16 00:48:27 +0200

    final app

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-header('Content-type: application/json');
 
 $time = (object)[];
 
 if($_SERVER['QUERY_STRING']) {
+header('Content-type: application/json');
 
 	$string_to_check = trim($_SERVER['QUERY_STRING']);
 
@@ -30,4 +30,44 @@ if($_SERVER['QUERY_STRING']) {
 	}
 
 	echo $time;
+} else { ?>
+
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Timestamp microservice</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="header">
+                PHP API Basejump: Timestamp microservice
+            </h1>
+            <blockquote>
+                User stories:
+                <ul>1) I can pass a string as a parameter, and it will check to see whether that string 
+                contains either a unix timestamp or a natural language date (example: January 1, 2016)</ul>
+                <ul>2) If it does, it returns both the Unix timestamp and the natural language form of that date.</ul>
+                <ul>3) If it does not contain a date or Unix timestamp, it returns null for those properties.</ul>
+            </blockquote>
+            <h3>Example usage:</h3>
+            <code>localhost/freecodecamp-timestamp_microservice/December%2015,%202015
+
+</code><br>
+            <code>localhost/freecodecamp-timestamp_microservice/1450137600
+
+</code>
+            <h3>Example output:</h3>
+            <code>
+                {
+                  "unix": 1450137600,
+                  "natural": "December 15, 2015"
+                }
+            </code>
+        </div>
+    </body>
+</html>
+
+<?php
 }
