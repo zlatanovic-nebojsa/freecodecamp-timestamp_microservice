71e7624fbfc514f63d677a594b08c593fda1e8b2
danneggiasta <zlatanovic.nebojsa@yahoo.com>
2017-09-16 01:33:23 +0200

    styling

--- index.php
+++ index.php
@@ -37,28 +37,31 @@ header('Content-type: application/json');
 <html lang="en">
     <head>
         <title>Timestamp microservice</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+     <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
     </head>
     <body>
         <div class="container">
             <h1 class="header">
                 PHP API Basejump: Timestamp microservice
             </h1>
-            <blockquote>
-                User stories:
-                <ul>1) I can pass a string as a parameter, and it will check to see whether that string 
-                contains either a unix timestamp or a natural language date (example: January 1, 2016)</ul>
-                <ul>2) If it does, it returns both the Unix timestamp and the natural language form of that date.</ul>
-                <ul>3) If it does not contain a date or Unix timestamp, it returns null for those properties.</ul>
+            <blockquote class="mt-5">
+                <p>User stories:</p>
+                <ul><li>I can pass a string as a parameter, and it will check to see whether that string 
+                contains either a unix timestamp or a natural language date (example: January 1, 2016)</li></ul>
+                <ul><li>If it does, it returns both the Unix timestamp and the natural language form of that date.</li></ul>
+                <ul><li>If it does not contain a date or Unix timestamp, it returns null for those properties.</li></ul>
             </blockquote>
-            <h3>Example usage:</h3>
+            <h3 class="mt-5">Example usage:</h3>
             <code>localhost/freecodecamp-timestamp_microservice-master/December%2015,%202015
 
 </code><br>
             <code>localhost/freecodecamp-timestamp_microservice-master/1450137600
 
 </code>
-            <h3>Example output:</h3>
+            <h3 class="mt-5">Example output:</h3>
             <code>
                 {
                   "unix": 1450137600,
